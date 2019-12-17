b6838e64bbd2e51523d47c9e7f6c0bbc217d08a9
AvilaAi <avila911008@gmail.com>
2019-12-17 03:45:22 +0100

    works on firefox and chrome

--- src/Piano.js
+++ src/Piano.js
@@ -1,7 +1,288 @@
 import React, { useState } from 'react';
-import { Button, Card, Box } from '@material-ui/core';
-import { FiberManualRecord, Stop } from '@material-ui/icons';
+import { Button, Box, Dialog, DialogTitle, DialogContent, DialogActions, TextField } from '@material-ui/core';
 
+// audio of each note
+let c3 = 'audio/c3.mp3';
+let c33 = 'audio/c33.mp3';
+let d3 = 'audio/d3.mp3';
+let d33 = 'audio/d33.mp3';
+let e3 = 'audio/e3.mp3';
+let f3 = 'audio/f3.mp3';
+let f33 = 'audio/f33.mp3';
+let g3 = 'audio/g3.mp3';
+let g33 = 'audio/g33.mp3';
+let a3 = 'audio/a3.mp3';
+let a33 = 'audio/a33.mp3';
+let b3 = 'audio/b3.mp3';
+let c4 = 'audio/c4.mp3';
+let c2 = 'audio/c2.mp3';
+let c22 = 'audio/c22.mp3';
+let d2 = 'audio/d2.mp3';
+let d22 = 'audio/d22.mp3';
+let e2 = 'audio/e2.mp3';
+let f2 = 'audio/f2.mp3';
+let f22 = 'audio/f22.mp3';
+let g2 = 'audio/g2.mp3';
+let g22 = 'audio/g22.mp3';
+let a2 = 'audio/a2.mp3';
+let a22 = 'audio/a22.mp3';
+let b2 = 'audio/b2.mp3';
+let s = 'audio/break.mp3';
+
+const allNotes = [
+	{
+		type: 'white',
+		name: 'C',
+		audio: c2,
+		key: 'S',
+	},
+	{
+		type: 'black',
+		name: 'C#',
+		audio: c22,
+		key: '1',
+	},
+	{
+		name: 'D',
+		type: 'white',
+		audio: d2,
+		key: 'D',
+	},
+	{
+		type: 'black',
+		name: 'D#',
+		audio: d22,
+		key: '2',
+	},
+	{
+		name: 'E',
+		type: 'white',
+		audio: e2,
+		key: 'F',
+	},
+	{
+		name: 'F',
+		type: 'white',
+		audio: f2,
+		key: 'G',
+	},
+	{
+		name: 'F#',
+		audio: f22,
+		type: 'black',
+		key: '3',
+	},
+	{
+		name: 'G',
+		type: 'white',
+		audio: g2,
+		key: 'H',
+	},
+	{
+		name: 'G#',
+		type: 'black',
+		audio: g22,
+		key: '4',
+	},
+	{
+		name: 'A',
+		type: 'white',
+		audio: a2,
+		key: 'J',
+	},
+	{
+		name: 'A#',
+		type: 'black',
+		audio: a22,
+		key: '5',
+	},
+	{
+		name: 'B',
+		type: 'white',
+		audio: b2,
+		key: 'K',
+	},
+	{
+		name: 'C',
+		type: 'white',
+		audio: c3,
+		key: 'L/E',
+	},
+	{
+		name: 'C#',
+		audio: c33,
+		type: 'black',
+		key: '6',
+	},
+	{
+		name: 'D',
+		type: 'white',
+		audio: d3,
+		key: 'R',
+	},
+	{
+		name: 'D#',
+		type: 'black',
+		audio: d33,
+		key: '7',
+	},
+	{
+		name: 'E',
+		type: 'white',
+		audio: e3,
+		key: 'T',
+	},
+	{
+		name: 'F',
+		type: 'white',
+		audio: f3,
+		key: 'Y',
+	},
+	{
+		name: 'F#',
+		audio: f33,
+		type: 'black',
+		key: '8',
+	},
+	{
+		name: 'G',
+		type: 'white',
+		audio: g3,
+		key: 'U',
+	},
+	{
+		name: 'G#',
+		type: 'black',
+		audio: g33,
+		key: '9',
+	},
+	{
+		name: 'A',
+		type: 'white',
+		audio: a3,
+		key: 'I',
+	},
+
+	{
+		name: 'A#',
+		type: 'black',
+		audio: a33,
+		key: '0',
+	},
+	{
+		name: 'B',
+		type: 'white',
+		audio: b3,
+		key: 'O',
+	},
+	{
+		name: 'C',
+		type: 'white',
+		audio: c4,
+		key: 'P',
+	},
+];
+// sheet chihiro
+const right1 = [e3, f33, g33, e3, b3, s, s, g33, f33, s, b3, s, f33, s, e3, c33, g33, s, s, e3, d33, s, s, s];
+const left1 = [s, s, e2, s, g22, s, b2, s, d22, s, b2, s, d33, s, c22, s, g22, s, c33, s, d22, g22, b2, s];
+const right2 = [e3, d33, c33, s, d33, s, e3, f33, b2, s, e3, s, f33, g33, a3, s, a3, g33, f33, e3, f33, s, s, s];
+const left2 = [s, s, a2, s, g22, s, c33, s, e2, s, g22, s, b2, s, f22, s, a2, s, c33, s, f22, b2, f33, s];
+const right3 = [e3, f33, g33, e3, b3, s, s, g33, f33, s, b3, s, f33, s, e3, c33, c33, s, d33, e3, b2, s, s, s, s];
+const left3 = [s, s, e2, s, b2, s, e3, s, d22, s, b2, s, d33, s, c22, s, e2, s, g22, s, g22, a2, b2, s, s, s];
+const right4 = [b2, c33, s, d33, s, e3, f33, b2, s, e3, s, f33, g33, a3, s, a3, g33, f33, e3, e3, s, s];
+const left4 = [s, a2, s, b2, s, g22, s, e2, s, g22, s, b2, s, f22, s, a2, s, c33, s, e2, b2, e3, s];
+// sheet twinkle
+const star1 = [d3, d3, a3, a3, b3, b3, a3, s, g3, g3, f33, f33, e3, e3, d3, s];
+const star2 = [a3, a3, g3, g3, f33, f33, e3, s, a3, a3, g3, g3, f33, f33, e3, s];
+const star11 = [d2, a2, f22, a2, g2, a2, f22, a2, e2, b2, a2, b2, g2, b2, f22, a2];
+const star22 = [d2, a2, g2, a2, f22, a2, e2, a2, d2, a2, g2, a2, f22, a2, e2, a2, s];
+const star3 = [d2, a2, f22, a2, g2, a2, f22, a2, e2, b2, a2, b2, g2, b2, d2, a2, d2, s];
+
+// sheet hp
+const harrypotter = [
+	a2,
+	d3,
+	f3,
+	e3,
+	d3,
+	a3,
+	g3,
+	g3,
+	e3,
+	e3,
+	d3,
+	f3,
+	e3,
+	c33,
+	d33,
+	a2,
+	a2,
+	a2,
+	a2,
+	d3,
+	f3,
+	e3,
+	d3,
+	a3,
+	c4,
+	b3,
+	a33,
+	f33,
+	a33,
+	a3,
+	g33,
+	a2,
+	f3,
+	d3,
+	d3,
+	d3,
+	s,
+];
+//tempo hp
+const tempo_harrypotter = [
+	0,
+	400,
+	549,
+	168,
+	380,
+	693,
+	460,
+	589,
+	432,
+	692,
+	398,
+	500,
+	143,
+	387,
+	651,
+	393,
+	712,
+	404,
+	756,
+	381,
+	594,
+	168,
+	380,
+	708,
+	385,
+	687,
+	440,
+	715,
+	403,
+	602,
+	143,
+	438,
+	717,
+	405,
+	715,
+	426,
+	1000,
+];
+
+// keyboard
+const keyboard = [83, 49, 68, 50, 70, 71, 51, 72, 52, 74, 53, 75, 69, 54, 82, 55, 84, 89, 56, 85, 57, 73, 48, 79, 80];
+
+// component MyPiano
 const MyPiano = props => {
 	const [left, setLeft] = useState(null);
 	const [right, setRight] = useState(null);
@@ -12,282 +293,11 @@ const MyPiano = props => {
 	const [record, setRecord] = useState(false);
 	const [showKeyboard, setShowKeyboard] = useState(false);
 	const [showHp, setShowHp] = useState(false);
+	const [popInfo, setPopInfo] = useState(false);
+	const [popSave, setPopSave] = useState(false);
+	const [savedSong, setSavedSong] = useState(null);
 
 	var decalage;
-	let c3 = 'audio/c3.mp3';
-	let c33 = 'audio/c33.mp3';
-	let d3 = 'audio/d3.mp3';
-	let d33 = 'audio/d33.mp3';
-	let e3 = 'audio/e3.mp3';
-	let f3 = 'audio/f3.mp3';
-	let f33 = 'audio/f33.mp3';
-	let g3 = 'audio/g3.mp3';
-	let g33 = 'audio/g33.mp3';
-	let a3 = 'audio/a3.mp3';
-	let a33 = 'audio/a33.mp3';
-	let b3 = 'audio/b3.mp3';
-	let c4 = 'audio/c4.mp3';
-	let c2 = 'audio/c2.mp3';
-	let c22 = 'audio/c22.mp3';
-	let d2 = 'audio/d2.mp3';
-	let d22 = 'audio/d22.mp3';
-	let e2 = 'audio/e2.mp3';
-	let f2 = 'audio/f2.mp3';
-	let f22 = 'audio/f22.mp3';
-	let g2 = 'audio/g2.mp3';
-	let g22 = 'audio/g22.mp3';
-	let a2 = 'audio/a2.mp3';
-	let a22 = 'audio/a22.mp3';
-	let b2 = 'audio/b2.mp3';
-	let s = 'audio/break.mp3';
-
-	const allNotes = [
-		{
-			type: 'white',
-			name: 'C',
-			audio: c2,
-			key: 'S',
-		},
-		{
-			type: 'black',
-			name: 'C#',
-			audio: c22,
-			key: '1',
-		},
-		{
-			name: 'D',
-			type: 'white',
-			audio: d2,
-			key: 'D',
-		},
-		{
-			type: 'black',
-			name: 'D#',
-			audio: d22,
-			key: '2',
-		},
-		{
-			name: 'E',
-			type: 'white',
-			audio: e2,
-			key: 'F',
-		},
-		{
-			name: 'F',
-			type: 'white',
-			audio: f2,
-			key: 'G',
-		},
-		{
-			name: 'F#',
-			audio: f22,
-			type: 'black',
-			key: '3',
-		},
-		{
-			name: 'G',
-			type: 'white',
-			audio: g2,
-			key: 'H',
-		},
-		{
-			name: 'G#',
-			type: 'black',
-			audio: g22,
-			key: '4',
-		},
-		{
-			name: 'A',
-			type: 'white',
-			audio: a2,
-			key: 'J',
-		},
-		{
-			name: 'A#',
-			type: 'black',
-			audio: a22,
-			key: '5',
-		},
-		{
-			name: 'B',
-			type: 'white',
-			audio: b2,
-			key: 'K',
-		},
-		{
-			name: 'C',
-			type: 'white',
-			audio: c3,
-			key: 'L/E',
-		},
-		{
-			name: 'C#',
-			audio: c33,
-			type: 'black',
-			key: '6',
-		},
-		{
-			name: 'D',
-			type: 'white',
-			audio: d3,
-			key: 'R',
-		},
-		{
-			name: 'D#',
-			type: 'black',
-			audio: d33,
-			key: '7',
-		},
-		{
-			name: 'E',
-			type: 'white',
-			audio: e3,
-			key: 'T',
-		},
-		{
-			name: 'F',
-			type: 'white',
-			audio: f3,
-			key: 'Y',
-		},
-		{
-			name: 'F#',
-			audio: f33,
-			type: 'black',
-			key: '8',
-		},
-		{
-			name: 'G',
-			type: 'white',
-			audio: g3,
-			key: 'U',
-		},
-		{
-			name: 'G#',
-			type: 'black',
-			audio: g33,
-			key: '9',
-		},
-		{
-			name: 'A',
-			type: 'white',
-			audio: a3,
-			key: 'I',
-		},
-
-		{
-			name: 'A#',
-			type: 'black',
-			audio: a33,
-			key: '0',
-		},
-		{
-			name: 'B',
-			type: 'white',
-			audio: b3,
-			key: 'O',
-		},
-		{
-			name: 'C',
-			type: 'white',
-			audio: c4,
-			key: 'P',
-		},
-	];
-
-	const right1 = [e3, f33, g33, e3, b3, s, s, g33, f33, s, b3, s, f33, s, e3, c33, g33, s, s, e3, d33, s, s, s];
-	const left1 = [s, s, e2, s, g22, s, b2, s, d22, s, b2, s, d33, s, c22, s, g22, s, c33, s, d22, g22, b2, s];
-	const right2 = [e3, d33, c33, s, d33, s, e3, f33, b2, s, e3, s, f33, g33, a3, s, a3, g33, f33, e3, f33, s, s, s];
-	const left2 = [s, s, a2, s, g22, s, c33, s, e2, s, g22, s, b2, s, f22, s, a2, s, c33, s, f22, b2, f33, s];
-	const right3 = [e3, f33, g33, e3, b3, s, s, g33, f33, s, b3, s, f33, s, e3, c33, c33, s, d33, e3, b2, s, s, s, s];
-	const left3 = [s, s, e2, s, b2, s, e3, s, d22, s, b2, s, d33, s, c22, s, e2, s, g22, s, g22, a2, b2, s, s, s];
-	const right4 = [b2, c33, s, d33, s, e3, f33, b2, s, e3, s, f33, g33, a3, s, a3, g33, f33, e3, e3, s, s];
-	const left4 = [s, a2, s, b2, s, g22, s, e2, s, g22, s, b2, s, f22, s, a2, s, c33, s, e2, b2, e3, s];
-
-	const star1 = [d3, d3, a3, a3, b3, b3, a3, s, g3, g3, f33, f33, e3, e3, d3, s];
-	const star2 = [a3, a3, g3, g3, f33, f33, e3, s, a3, a3, g3, g3, f33, f33, e3, s];
-	const star11 = [d2, a2, f22, a2, g2, a2, f22, a2, e2, b2, a2, b2, g2, b2, f22, a2];
-	const star22 = [d2, a2, g2, a2, f22, a2, e2, a2, d2, a2, g2, a2, f22, a2, e2, a2, s];
-	const star3 = [d2, a2, f22, a2, g2, a2, f22, a2, e2, b2, a2, b2, g2, b2, d2, a2, d2, s];
-
-	const harrypotter = [
-		a2,
-		d3,
-		f3,
-		e3,
-		d3,
-		a3,
-		g3,
-		g3,
-		e3,
-		e3,
-		d3,
-		f3,
-		e3,
-		c33,
-		d33,
-		a2,
-		a2,
-		a2,
-		a2,
-		d3,
-		f3,
-		e3,
-		d3,
-		a3,
-		c4,
-		b3,
-		a33,
-		f33,
-		a33,
-		a3,
-		g33,
-		a2,
-		f3,
-		d3,
-		d3,
-		d3,
-		s,
-	];
-	const tempo_harrypotter = [
-		0,
-		400,
-		549,
-		168,
-		380,
-		693,
-		460,
-		589,
-		432,
-		692,
-		398,
-		500,
-		143,
-		387,
-		651,
-		393,
-		712,
-		404,
-		756,
-		381,
-		594,
-		168,
-		380,
-		708,
-		385,
-		687,
-		440,
-		715,
-		403,
-		602,
-		143,
-		438,
-		717,
-		405,
-		715,
-		426,
-		1000,
-	];
 
 	// play single note
 	const playAudio = async (name, event) => {
@@ -324,9 +334,11 @@ const MyPiano = props => {
 		audio.volume = 0.5;
 		audio.play();
 	};
+
 	var offset = 0;
 	var count = 0;
 
+	// play harry potter
 	const playHp = () => {
 		playAudio(harrypotter[count]);
 		if (count < harrypotter.length) {
@@ -339,6 +351,7 @@ const MyPiano = props => {
 		}
 	};
 
+	// play twinkle
 	const playStar = () => {
 		for (let i = 0; i < star1.length; i++) {
 			setTimeout(() => {
@@ -363,7 +376,7 @@ const MyPiano = props => {
 		}
 	};
 
-	//chihiro
+	// play chihiro
 	const playSong = () => {
 		for (let i = 0; i < right1.length; i++) {
 			setTimeout(() => {
@@ -412,6 +425,7 @@ const MyPiano = props => {
 			setTime(null);
 		}
 	};
+
 	// play recorded song
 	const playNewSong = () => {
 		for (let i = 0; i < newSong.length; i++) {
@@ -422,38 +436,24 @@ const MyPiano = props => {
 		}
 	};
 
-	// keyboard
-	const keyboard = [
-		83,
-		49,
-		68,
-		50,
-		70,
-		71,
-		51,
-		72,
-		52,
-		74,
-		53,
-		75,
-		69,
-		54,
-		82,
-		55,
-		84,
-		89,
-		56,
-		85,
-		57,
-		73,
-		48,
-		79,
-		80,
-	];
+	const saveTheSong = () => {
+		setSavedSong({ ...savedSong, tempo: tempo, sheet: newSong });
+		console.log('savedSong', savedSong);
+		setPopSave(false);
+	};
 
+	const playSavedSong = () => {
+		for (let i = 0; i < savedSong.sheet.length; i++) {
+			setTimeout(() => {
+				playAudio(savedSong.sheet[i]);
+			}, savedSong.tempo[i] + offset);
+			offset += savedSong.tempo[i];
+		}
+	};
+	// function keyboard
 	document.onkeydown = e => {
 		var keyNum = window.event ? e.keyCode : e.which;
-		if (e.repeat) return;
+		if (e.repeat || popSave) return;
 		const index = keyboard.indexOf(keyNum);
 		if (keyNum === 76) {
 			playAudio(allNotes[12].audio, e);
@@ -462,33 +462,39 @@ const MyPiano = props => {
 			playAudio(allNotes[index].audio, e);
 		}
 	};
-	const tips = 'JRYTRIUTRYT67JJRYTRIPO080I94YR';
+	const tipsHp = 'JRYTRIUTRYT67JJRYTRIPO080I94YR';
 
 	return (
 		<div className='page'>
 			<div className='tips'>
 				{showHp &&
-					[...tips].map(p => {
+					[...tipsHp].map(p => {
 						return <span className='tip'>{p}</span>;
 					})}
 			</div>
-			<div className='groupeBouton'>
-				<Button className='bouton' onClick={() => setShowKeyboard(!showKeyboard)}>
-					{showKeyboard ? ' 💻  Hide keyboard note' : '💻   Show keyboard note'}
-				</Button>
-				<Button className='bouton' onClick={() => setShowHp(!showHp)}>
-					<>👩🏻‍🏫</> Learn a song
-				</Button>
-
-				<Button className='bouton' id={record && 'recording'} onClick={clickRecord}>
-					{record ? '⬛️ stop' : '🔴 record'}
-				</Button>
-				<Button className='bouton' onClick={() => playNewSong()}>
-					<> 🎹</> Play{' '}
-				</Button>
-				<Button className='bouton' onClick={() => setShowHp(!showHp)}>
-					<>☑️</> Save
-				</Button>
+			<div className='head'>
+				<div className='groupeBouton'>
+					<Button
+						className={showKeyboard ? 'bouton active' : 'bouton showkeboard'}
+						onClick={() => setShowKeyboard(!showKeyboard)}
+					>
+						{showKeyboard ? ' 💻  Hide keyboard note' : '💻   Show keyboard note'}
+					</Button>
+					<Button onClick={() => setShowHp(!showHp)}>
+						<>🔎</> Learn a song
+					</Button>
+				</div>
+				<div className='groupeBouton'>
+					<Button id={record && 'recording'} onClick={clickRecord}>
+						{record ? '⏹ Stop' : '🔴 Record'}
+					</Button>
+					<Button onClick={() => playNewSong()} disabled={!record && newSong.length > 0 ? false : true}>
+						<> ▶️</> Play{' '}
+					</Button>
+					<Button onClick={() => setPopSave(true)} disabled={!record && newSong.length > 0 ? false : true}>
+						<>☑️</> Save
+					</Button>
+				</div>
 			</div>
 
 			{allNotes.map(note => {
@@ -517,19 +523,77 @@ const MyPiano = props => {
 				})}
 			</div>
 
-			<div className='groupeBouton'>
-				<Box className='box'>Demo songs :</Box>
+			<div className='foot'>
+				<div className='groupeBouton'>
+					<Box className='box'>Demo songs :</Box>
 
-				<Button className='bouton' onClick={() => playSong()}>
-					🌸 Spirited Away{' '}
-				</Button>
-				<Button className='bouton' onClick={() => playStar()}>
-					⭐️ Twinkle twinkle{' '}
-				</Button>
-				<Button className='bouton' onClick={() => playHp()}>
-					⚡️ Harry potter Hedwig's theme{' '}
-				</Button>
+					<Button onClick={() => playSong()}>
+						<>🌸</> Spirited Away{' '}
+					</Button>
+					<Button onClick={() => playStar()}>
+						<>⭐️</> Twinkle twinkle{' '}
+					</Button>
+					<Button onClick={() => playHp()}>
+						<>⚡️ </>Harry Potter Hedwig's theme{' '}
+					</Button>
+				</div>
+				<div className='groupeBouton'>
+					<Box className='box'>Your song :</Box>
+
+					{savedSong && savedSong.sheet ? (
+						<Button onClick={() => playSavedSong()}>
+							<>👏🏼</>
+							{savedSong.name}
+						</Button>
+					) : (
+						<Button onClick={() => setPopInfo(true)}>
+							<> 🙋🏼</> Create your own song
+						</Button>
+					)}
+				</div>
 			</div>
+
+			<Dialog open={popInfo} onClose={() => setPopInfo(false)}>
+				<DialogTitle>
+					{' '}
+					<>💡 </> How to create your own song :
+				</DialogTitle>
+				<DialogContent>
+					<h4>1. click -Record- to start recording </h4>
+					<h4>2. click -Stop- to finish recording </h4>
+					<h4>3. click -Play- to check it</h4>
+					<h4>4. click -Save- to name it and save</h4>
+
+					<DialogActions>
+						<Button onClick={() => setPopInfo(false)}>
+							{' '}
+							<> 👌🏼 </>Got it !
+						</Button>
+					</DialogActions>
+				</DialogContent>
+			</Dialog>
+
+			<Dialog open={popSave} onClose={() => setPopSave(false)}>
+				<DialogTitle>Name your song and save :</DialogTitle>
+				<DialogContent>
+					<TextField
+						fullWidth
+						InputLabelProps={{
+							shrink: true,
+						}}
+						id='outlined-basic'
+						label='Tape here the name you like'
+						variant='outlined'
+						onChange={e => setSavedSong({ ...savedSong, name: e.target.value })}
+						margin='normal'
+					></TextField>
+					<DialogActions>
+						<Button onClick={saveTheSong}>
+							<>✌🏼</>Done !
+						</Button>
+					</DialogActions>
+				</DialogContent>
+			</Dialog>
 		</div>
 	);
 };
